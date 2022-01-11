3ebd7d540e4b6852fe3b2464002113986c7d23ff
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-01-11 21:38:27 +0100

    setup some error handling for weather forecast

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const request = require('request')
 
-const url = "http://api.weatherstack.com/current?access_key=4d126ad5c1a186bf6e5841ba892f60dd&query=New%20York&units=f"
+const url = "http://api.weatherstack.com/current?access_key=4d126ad5c1a186bf6e5841ba892f60dd&query=37.8267,-122.4233&units=f"
 
 request({url: url, json: true}, (error, response) => {
-  console.log(` ${response.body.current.weather_descriptions[0]}. it is ${response.body.current.temperature} farenheit degree, it feelslike ${response.body.current.feelslike} Farenheit `)
-  // const data = JSON.parse(response.body) thanks do json: true we don't need that
-  // console.log(data.current)
+  if (error) {
+      console.log('Unable to connect to weather service!');
+  } else if (response.body.error) {
+      console.log('Unable to find location');
+  } else {
+      console.log(` ${response.body.current.weather_descriptions[0]}. it is ${response.body.current.temperature} farenheit degree, it feelslike ${response.body.current.feelslike} Farenheit `);
+     // const data = JSON.parse(response.body) thanks do json: true we don't need that
+     // console.log(data.current)
+  }
+
 })
 
-//geocoding
-// address -> lat/long -> weather
-const urlGeo = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoianVsaWVubWFsIiwiYSI6ImNrd3Z3M3Z0MzFtN2IzMWxzdzFtNG0xdTEifQ.mI8ofvz_w0NV4p9AtYoOqw&limit=1"
-request({url: urlGeo, json: true}, (error, response) => {
-  console.log(`${response.body.features[0].center[0]}, ${response.body.features[0].center[1]}`)
-})
+
+// const urlGeo = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoianVsaWVubWFsIiwiYSI6ImNrd3Z3M3Z0MzFtN2IzMWxzdzFtNG0xdTEifQ.mI8ofvz_w0NV4p9AtYoOqw&limit=1"
+// request({url: urlGeo, json: true}, (error, response) => {
+//   const latitude = response.body.features[0].center[1]
+//   const longitude = response.body.features[0].center[0]
+//   console.log(latitude, longitude)
+// })
