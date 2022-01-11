50a0f54e470d518f2bb1fdd9cb901a55c9ba1aba
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-01-11 21:27:28 +0100

    setup for geocoding

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const request = require('request')
 
-const url = "http://api.weatherstack.com/current?access_key=4d126ad5c1a186bf6e5841ba892f60dd&query=New%20York"
+const url = "http://api.weatherstack.com/current?access_key=4d126ad5c1a186bf6e5841ba892f60dd&query=New%20York&units=f"
 
-request({url: url}, (error, response) => {
-  const data = JSON.parse(response.body)
-  console.log(data.current)
+request({url: url, json: true}, (error, response) => {
+  console.log(` ${response.body.current.weather_descriptions[0]}. it is ${response.body.current.temperature} farenheit degree, it feelslike ${response.body.current.feelslike} Farenheit `)
+  // const data = JSON.parse(response.body) thanks do json: true we don't need that
+  // console.log(data.current)
+})
+
+//geocoding
+// address -> lat/long -> weather
+const urlGeo = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoianVsaWVubWFsIiwiYSI6ImNrd3Z3M3Z0MzFtN2IzMWxzdzFtNG0xdTEifQ.mI8ofvz_w0NV4p9AtYoOqw&limit=1"
+request({url: urlGeo, json: true}, (error, response) => {
+  console.log(`${response.body.features[0].center[0]}, ${response.body.features[0].center[1]}`)
 })
