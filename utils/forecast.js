05c2c7f94a029b34e4f565bdd0fd4e53f7961f5d
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-01-11 23:14:09 +0100

    linked both api

--- utils/forecast.js
+++ utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 
 const forecast = (latitude, longitude, callback) => {
-  const url = 'http://api.weatherstack.com/current?access_key=4d126ad5c1a186bf6e5841ba892f60dd&query=lat=' + latitude + '&long=' + longitude + '&units=f'
+  const url = 'http://api.weatherstack.com/current?access_key=4d126ad5c1a186bf6e5841ba892f60dd&query=' + latitude + ','+ longitude
 
   request({url: url, json: true}, (error, response) => {
     if (error) {
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
     } else if (response.body.error) {
         callback('Unable to find location', undefined);
     } else {
-        callback(undefined, response.body.current.weather_descriptions[0] + 'it is' + response.body.current.temperature + 'farenheit degree, it feelslike' + response.body.current.feelslike + 'Farenheit')
+        callback(undefined, response.body.current.weather_descriptions[0] + ' it is ' + response.body.current.temperature + ' degree, it feels like ' + response.body.current.feelslike + ' degree')
     }
   })
 }
